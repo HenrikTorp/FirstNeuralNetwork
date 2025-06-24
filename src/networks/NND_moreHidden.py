f5ee6a27badb14a6e912880e22295c1b55548e88
Henrik <thkodehode@gmail.com>
2025-06-24 11:19:12 +0200

    small comments

--- src/networks/NND_moreHidden.py
+++ src/networks/NND_moreHidden.py
@@ -9,8 +9,8 @@ def sigmoid_derivative(x):
     return x * (1 - x)
 
 def softmax(x):
-    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))  # Numerical stability
-    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))  # numerical stability
+    return exp_x / np.sum(exp_x, axis=1, keepdims=True) # mathematically equivalent because exp(x) on both sides of the fraction
 
 class NNDNetworkV2:
     def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
@@ -19,7 +19,9 @@ class NNDNetworkV2:
         self.hidden_size2 = hidden_size2
         self.output_size = output_size
 
-        # Xavier Initialization
+        # Xavier Initialization, first normally distributed random numbers of mean 0 and variance 1 
+        # multiplied by sqrt(2/(input_size + output_size)) for each layer
+        # Keeps the range of the weights small in accordance with the number of inputs and outputs, which helps in faster convergence
         self.weights_input_hidden1 = np.random.randn(self.input_size, self.hidden_size1) * np.sqrt(2 / (self.input_size+self.hidden_size1))
         self.weights_hidden1_hidden2 = np.random.randn(self.hidden_size1, self.hidden_size2) * np.sqrt(2 / (self.hidden_size1 + self.hidden_size2))
         self.weights_hidden2_output = np.random.randn(self.hidden_size2, self.output_size) * np.sqrt(2/ (self.hidden_size2 + self.output_size))
@@ -74,11 +76,11 @@ class NNDNetworkV2:
     The math of backward propagation involves applying the chain rule to compute the gradient of the loss function
     with respect to each weight and bias in the network. Matrix operations are used to efficiently propagate these
     gradients backward through the network, layer by layer, for all samples in a batch simultaneously. This allows
-    us to update the weights and biases to minimize the loss function during training.
+    updating the weights and biases to minimize the loss function during training.
         """
         
         
-        delta_3 = y_pred - y_true # Gradient of the loss with respect to the output
+        delta_3 = y_pred - y_true # derivative of softmax loss function with respect to the output layer
         d_weights_hidden2_output = np.dot(self.hidden_layer2_output.T, delta_3) # Gradient of the loss with respect to the weights (weights being a matrix of shape (hidden_size2, output_size))
         d_bias_output = np.sum(delta_3, axis=0, keepdims=True) # Gradient of the loss with respect to the bias (bias being a vector of shape (output_size,))
 
